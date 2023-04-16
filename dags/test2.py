78dcbc8f9fc27d2ad3856ef8f170debca96480c8
Amir <amir.zahreddine@gmail.com>
2023-04-16 14:45:12 -0600

    cd_test changed test2

--- dags/test2.py
+++ dags/test2.py
@@ -39,7 +39,7 @@ with DAG(
 
     # t1, t2 and t3 are examples of tasks created by instantiating operators
     t1 = BashOperator(
-        task_id="print_date_2",
+        task_id="print_date_233",
         bash_command="date",
     )
 
